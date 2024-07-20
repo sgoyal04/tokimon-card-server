e157af0cdcd32c372768f845d74558e319ff1215
Gurveen <gurveennanua@gmail.com>
2024-07-20 12:47:05 -0700

    initial commit

---
+++ src/main/java/ca/cmpt213/asn5/Asn5Application.java
@@ -0,0 +1,13 @@
+package ca.cmpt213.asn5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asn5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Asn5Application.class, args);
+    }
+
+}
---
+++ src/test/java/ca/cmpt213/asn5/Asn5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cmpt213.asn5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
