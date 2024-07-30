46b49efef313a509004cfd5232796b3fefac36d6
Gurveen <gurveennanua@gmail.com>
2024-07-30 13:33:03 -0700

    tests updated

--- src/test/java/ca/cmpt213/asn5/controllers/TokimonControllerTest.java
+++ src/test/java/ca/cmpt213/asn5/controllers/TokimonControllerTest.java
@@ -1,90 +1,105 @@
 package ca.cmpt213.asn5.controllers;
 
 import ca.cmpt213.asn5.models.Tokimon;
-import ca.cmpt213.asn5.models.TokimonList;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
 
-
-@WebMvcTest(TokimonController.class)
-class TokimonControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TokimonControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private TokimonList tokimonList;
+    @Autowired
+    private TokimonController tokimonController;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    private Tokimon tokimon;
 
     @BeforeEach
     public void setup() {
-        tokimon = new Tokimon("https://images.app.goo.gl/3dWRCMEXLvGaHyKS8", "pikachu", "electric",6);
-        tokimon.setTid(1);
+        System.out.println("Runs before each test");
     }
 
     @Test
-    public void testGetTokimonList() throws Exception {
-        List<Tokimon> allTokimons = Collections.singletonList(tokimon);
-        when(tokimonList.getTokimons()).thenReturn(allTokimons);
+    void contextLoads() {
+        assertThat(tokimonController).isNotNull();
+        assertThat(mockMvc).isNotNull();
+    }
 
+    @Test
+    public void testGetAllTokimons() throws Exception {
         mockMvc.perform(get("/tokimon/all"))
                 .andExpect(status().isOk());
-
     }
 
     @Test
-    public void testGetTokimon() throws Exception {
-        when(tokimonList.getTokimonWithTid(1)).thenReturn(tokimon);
-
-        mockMvc.perform(get("/tokimon/1"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void testAddTokimon() throws Exception {
-        Tokimon newTokimon = new Tokimon("https://images.app.goo.gl/DZkWVKU37ZY4bGkK7", "snorlax", "normal", 8);
-        mockMvc.perform(post("/tokimon/add")
+    public void testAddTokimonAndGetTokimon() throws Exception {
+        Tokimon newTokimon = new Tokimon("https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png", "pikachu", "electric", 6);
+        MvcResult result = mockMvc.perform(post("/tokimon/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newTokimon)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.imagePath", is("https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png")))
+                .andExpect(jsonPath("$.name", is("pikachu")))
+                .andExpect(jsonPath("$.type", is("electric")))
+                .andExpect(jsonPath("$.rarityScore", is(6)))
+                .andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        Tokimon createdTokimon = objectMapper.readValue(jsonResponse, Tokimon.class);
+        long tid = createdTokimon.getTid();
+        mockMvc.perform(get("/tokimon/{tid}", tid))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tid", is((int) tid)))
+                .andExpect(jsonPath("$.imagePath", is("https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png")))
+                .andExpect(jsonPath("$.name", is("pikachu")))
+                .andExpect(jsonPath("$.type", is("electric")))
+                .andExpect(jsonPath("$.rarityScore", is(6)));
+
+        // Delete the newly added Tokimon
+        mockMvc.perform(delete("/tokimon/{tid}", tid))
+                .andExpect(status().isNoContent());
     }
+
 
     @Test
     public void testDeleteTokimon() throws Exception {
-        doNothing().when(tokimonList).deleteTokimon(1);
+        Tokimon delTokimon = new Tokimon("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png", "squirtle", "water", 7);
+        long tid = delTokimon.getTid();
 
-        mockMvc.perform(delete("/tokimon/1"))
+        mockMvc.perform(delete("/tokimon/{tid}", tid))
                 .andExpect(status().isNoContent());
     }
 
     @Test
-    public void testUpdateTokimon() throws Exception {
-        Tokimon updatedTokimon = new Tokimon("https://images.app.goo.gl/QqKzkXpCaHVk3XYV6", "richu", "electric", 10);
-        updatedTokimon.setTid(1);
-        updatedTokimon.setName("richu");
-
-        mockMvc.perform(put("/tokimon/edit/1")
+    public void testUpdateTokimon() throws  Exception {
+        Tokimon updateTokimon = new Tokimon("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png", "squirtle", "water", 7);
+        updateTokimon.setType("air");
+        long tid = updateTokimon.getTid();
+        mockMvc.perform(put("/tokimon/edit/{tid}", tid)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedTokimon)))
-                .andExpect(status().isOk());
+                        .content(objectMapper.writeValueAsString(updateTokimon)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tid", is((int) tid)))
+                .andExpect(jsonPath("$.name", is("squirtle")))
+                .andExpect(jsonPath("$.type", is("air")))
+                .andExpect(jsonPath("$.rarityScore", is(7)));
     }
+
 }
